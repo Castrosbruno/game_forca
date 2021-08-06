a4b6bfa50616861b194cb6718afb1924cd5e479e
castrosbruno <bruno.2606@hotmail.com>
2021-08-05 21:25:42 -0300

    arquivos iniciais do projeto

---
+++ main.rb
@@ -0,0 +1,3 @@
+require_relative 'game_forca'
+
+jogo_da_forca
---
+++ ui.rb
@@ -0,0 +1,103 @@
+def avisa_campeao_atual(dados)
+  puts "Nossa campeão atual é #{dados[0]} com #{dados[1]} pontos."
+end
+
+def avisa_pontos_totais(pontos_totais)
+  puts "Você possui #{pontos_totais} pontos."
+end
+def avisa_chute_efetuado chute
+   puts "Você já chutou #{chute}"
+end
+
+def avisa_letra_nao_encontrada
+    puts "Letra não encontrada."
+end
+
+def letra_foi_encontrada(total_encontrado)
+  puts "Letra encontrada #{total_encontrado} vezes."
+end
+
+def acertou_palavra
+  puts "Parabéns você acertou!!"
+end
+
+def avisa_errou_palavra
+  puts "Que pena... Errou!!"
+end
+
+def avisa_pontos(pontos_ate_agora)
+  puts "Você ganhou #{pontos_ate_agora} pontos."
+end
+
+def da_boas_vindas
+  puts "/*************/"
+  puts "/JOGO DA FORCA*/"
+  puts "/*************/\n\n"
+  puts "Qual é o seu nome?"
+  nome = gets.strip
+  puts "\n\n\n\n\n"
+  puts "Começaremos o jogo para você, #{nome}!!"
+  nome
+end
+
+def desenha_forca(erros)
+  cabeca = "   "
+  bracos = "   "
+  pernas = "   "
+  corpo = " "
+  if erros >= 1
+    cabeca = "(_)"
+  end
+  if erros >= 2
+    bracos = " | "
+    corpo = " | "
+  end
+  if erros >= 3
+    bracos = "\\|/"
+  end
+  if erros >= 4
+    pernas = "/ \\"
+  end
+  puts " _________     \n"
+  puts " |/       |    \n"
+  puts " |       #{cabeca}    \n"
+  puts " |       #{bracos}    \n"
+  puts " |        #{corpo}        \n"
+  puts " |       #{pernas}    \n"
+  puts " |             \n"
+  puts "_|___          \n"
+  puts "\n\n"
+end
+
+def avisa_escolhendo_palavra
+  puts "Escolhendo uma palavra secreta..."
+end
+
+def avisa_palavra_escolhida(palavra_secreta)
+  puts "Palavra secreta com #{palavra_secreta.size} letras ... boa sorte!!"
+  palavra_secreta
+end
+
+def nao_quer_jogar
+  puts "Deseja jogar novamente? (S/N)"
+  quero_jogar = gets.strip
+  nao_quero_jogar = quero_jogar.upcase == "N"
+end
+
+def cabecalho_de_tentativa(chutes, erros, mascara)
+  puts "\n\n\n\n"
+  desenha_forca erros
+  puts "Palavra secreta é a #{mascara}"
+  puts "Erros até agora: #{erros}"
+  puts "Chutes até agora: #{chutes}"
+end
+
+def pede_um_chute
+  puts "Entre com uma letra ou uma palavra"
+  chute = gets.strip.downcase
+  puts "Será que acertou? Você chutou #{chute}"
+  chute
+end
+
+#irb - texto = File.read("dicionario.txt")
+# para quebrar a linha é texto.split somente nas quebrar de linha "\n"
